8b5ff9cf0650374763bc714561ca32a07450e979
Erik Koppenhagen <erik.koppenhagen@iits-consulting.de>
2024-12-12 09:17:39 +0100

    feat: add day 12 of 2024 (not implemented)

---
+++ src/main/kotlin/io/github/ekoppenhagen/aoc/year2024/Day12.kt
@@ -0,0 +1,12 @@
+package io.github.ekoppenhagen.aoc.year2024
+
+import io.github.ekoppenhagen.aoc.AbstractAocDay
+
+class Day12 : AbstractAocDay(day = 12) {
+
+    override fun solvePart1(gardenPlotMap: List<String>) =
+        "not implemented"
+
+    override fun solvePart2(gardenPlotMap: List<String>) =
+        "not implemented"
+}
