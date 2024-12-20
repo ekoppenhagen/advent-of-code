9bbf1f78200ea9a1aef542a0d833d19e6e90dba5
Erik Koppenhagen <erik.koppenhagen@iits-consulting.de>
2024-12-20 12:14:26 +0100

    chore: add notContains(x) to List extensions

--- src/main/kotlin/io/github/ekoppenhagen/aoc/extensions/Lists.kt
+++ src/main/kotlin/io/github/ekoppenhagen/aoc/extensions/Lists.kt
@@ -1,5 +1,8 @@
 package io.github.ekoppenhagen.aoc.extensions
 
+fun <E> List<E>.notContains(element: E) =
+    this.none { it == element }
+
 fun <E> List<E>.toPairOfFirstAndLastElement() =
     if (this.isEmpty()) null to null else this[0] to this[this.lastIndex]
 
