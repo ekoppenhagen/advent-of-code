31f45bc72cb276a73cf9e2398282aa1a714deaa5
Erik Koppenhagen <erik.koppenhagen@iits-consulting.de>
2024-12-20 13:11:25 +0100

    chore: add AoC day template

---
+++ src/main/kotlin/io/github/ekoppenhagen/aoc/TemplateDay.kt
@@ -0,0 +1,18 @@
+package io.github.ekoppenhagen.aoc
+
+suspend fun main() {
+    TemplateDay().solve()
+}
+
+class TemplateDay : AbstractAocDay(
+    exampleResultPart1 = null,
+    exampleResultPart2 = null,
+    solveParts = false,
+) {
+
+    override suspend fun solvePart1(lines: List<String>) =
+        "not implemented"
+
+    override suspend fun solvePart2(lines: List<String>) =
+        "not implemented"
+}
